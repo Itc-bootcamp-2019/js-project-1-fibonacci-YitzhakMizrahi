9d5e30d85f0b8ddf27fe8fb0668b5735fd6af04a
YitzhakMizrahi <mizrahi.yitzchak@gmail.com>
2020-03-03 15:45:47 +0200

    Milestone-4.0 (removed console.log)

--- Fibonacci/main.js
+++ Fibonacci/main.js
@@ -6,11 +6,10 @@ function fibonacci(x) {
   let first = 0,
     second = 1,
     next = 1;
-  for (let i = 1; i <= x; i++) {
+  for (let i = 2; i <= x; i++) {
     next = first + second;
     first = second;
     second = next;
-    console.log(next);
   }
   document.getElementById("fiboResult").innerText = `${next}`;
 }
