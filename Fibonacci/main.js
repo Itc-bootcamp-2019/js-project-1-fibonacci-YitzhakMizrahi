f4a2e9cb870663e1086f2593cfe9ffff06602ebb
YitzhakMizrahi <mizrahi.yitzchak@gmail.com>
2020-03-03 22:02:38 +0200

    Milestone-4.1 (combined if statements)

--- Fibonacci/main.js
+++ Fibonacci/main.js
@@ -1,9 +1,6 @@
 function fibonacci(x) {
-  if (x == 0) {
-    return (x = 0);
-  }
-  if (x == 1) {
-    return (x = 1);
+  if (x == 0 || x == 1) {
+    return x;
   } else {
     return (x = fibonacci(x - 1) + fibonacci(x - 2));
   }
