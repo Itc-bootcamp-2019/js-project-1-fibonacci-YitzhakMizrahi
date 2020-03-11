188613f29d6022f190cca995af320a9dfbeac086
Yitzhak <mizrahi.yitzchak@gmail.com>
2020-03-11 21:23:53 +0200

    Milestone-6

--- Fibonacci/main.js
+++ Fibonacci/main.js
@@ -1,12 +1,65 @@
-// fiboCalc = receives an input from the user
-// Sends the input to the server for calculation
+// fibCalc = receives an input from the user
+// If the input is < 50 sends the input to the server for calculation
 // Receives the result from the server and prints it to the user
-function fiboCalc() {
-  let input = document.getElementById("fiboInput").value;
+// If the input is >= 50 does not send it to the server and prints an error
 
-  fetch(`http://localhost:5050/fibonacci/${input}`)
-    .then(res => res.json())
-    .then(data => {
-      document.getElementById("fiboResult").innerText = data.result;
+function fibCalc() {
+  let input = document.getElementById("fibInput").value;
+  if (input >= 50) {
+    clearResults();
+    showLoader();
+    hideLoader();
+
+    const fibFiftyOrMore = document.getElementById("fibFiftyOrMore");
+    fibFiftyOrMore.classList.add("fib-fifty-or-more");
+    fibFiftyOrMore.innerText = "Can't be larger than 50";
+
+    const fibInput = document.getElementById("fibInput");
+    fibInput.classList.add("form-error");
+  } else {
+    fetch(`http://localhost:5050/fibonacci/${input}`).then(response => {
+      clearResults();
+      showLoader();
+      hideLoader();
+      if (response.ok) {
+        response.json().then(data => {
+          document.getElementById("fibResult").innerText = data.result;
+        });
+      } else {
+        response.text().then(text => {
+          document.getElementById("fibError").innerText = text;
+        });
+      }
     });
+  }
+}
+
+//Clears the previous results for all possibilities
+function clearResults() {
+  const fibFiftyOrMore = document.getElementById("fibFiftyOrMore");
+  fibFiftyOrMore.classList.remove("fib-fifty-or-more");
+  fibFiftyOrMore.innerText = "";
+
+  const fibInput = document.getElementById("fibInput");
+  fibInput.classList.remove("form-error");
+
+  document.getElementById(
+    "fibResult"
+  ).innerHTML = `<div class="col-6 result" id="fibResult"><span class="col-6 error-42" id="fibError"></span></div>`;
+}
+
+//Adds css class to present Loader
+function showLoader() {
+  setTimeout(function() {
+    const loader = document.getElementById("loader");
+    loader.classList.add("loader");
+  }, 0);
+}
+
+//Removes Loader css class in 2 seconds
+function hideLoader() {
+  setTimeout(function() {
+    const loader = document.getElementById("loader");
+    loader.classList.remove("loader");
+  }, 2000);
 }
